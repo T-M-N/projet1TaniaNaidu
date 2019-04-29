3ba87bf7006be1cde25c1de1c4f6f6b9f5a477dc
T-M-N <naidutania@outlook.fr>
2019-04-29 15:36:53 +0200

    Recherche artiste ok

--- js/recherche.js
+++ js/recherche.js
@@ -1,24 +1,22 @@
 const formSearch = $('#FormulaireRecherche');
-const recherche = $(formSearch).find('#recherche');
-
-$(formSearch).on('submit', function(event) {
+formSearch.on('submit', function(event) {
     event.preventDefault();
+    
+    const recherche = formSearch.find('#recherche');
+    const rechercheValeur= recherche.val();
 
-    const rech= $(recherche).val();
 
-   alert(rech);
-
+    $.ajax({
+        url : 'https://api.deezer.com/search?q='+rechercheValeur+'&output=jsonp',
+        dataType : 'jsonp'
+    }).done(function(musiques) {
+    
+        console.log(musiques);
+    
+        document.querySelector('#results').innerHTML =
+                musiques.data.map(m => m.title).join('<br>');
+    
+    });
 });
 
 
-$.ajax({
-    url : 'https://api.deezer.com/search?q=eminem&output=jsonp',
-    dataType : 'jsonp'
-}).done(function(musiques) {
-
-    console.log(musiques);
-
-    document.querySelector('#results').innerHTML =
-            musiques.data.map(m => m.title).join('<br>');
-
-});
