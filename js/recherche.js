c33a5f32f7af6577a8e9a45b1b4039c6a6ee83df
T-M-N <naidutania@outlook.fr>
2019-04-29 21:37:39 +0200

    Update recherche.js

--- js/recherche.js
+++ js/recherche.js
@@ -1,22 +1,36 @@
-const formSearch = $('#FormulaireRecherche');
-formSearch.on('submit', function(event) {
+const FormulaireRecherche = $('#FormulaireRecherche');
+let select = $("#select");
+
+FormulaireRecherche.on('submit', function(event) {
     event.preventDefault();
     
-    const recherche = formSearch.find('#recherche');
-    const rechercheValeur= recherche.val();
+    const recherche = FormulaireRecherche.find('#recherche');
+    const rechercheValeur = recherche.val();
 
+    let selectValeur = $( "select option:selected" ).val();
+    console.log(selectValeur);
 
     $.ajax({
-        url : 'https://api.deezer.com/search?q='+rechercheValeur+'&output=jsonp',
+        url : 'https://api.deezer.com/search?q='+rechercheValeur+'&order=RATING_ASC&output=jsonp',
         dataType : 'jsonp'
     }).done(function(musiques) {
     
         console.log(musiques);
     
-        document.querySelector('#results').innerHTML =
-                musiques.data.map(m => m.title).join('<br>');
+        document.querySelector('#results').innerHTML = 
+     /* "<p> Les titre : " + musiques.data.map(m => m.title)
+      + "</p><p> Les album : " + musiques.data.map(m => m.album.title) + "</p>";*/
+
+       musiques.data.map(
+            m => 
+            '<div class="main"><p class="album">'
+             + m.album.cover_big + '<br>'
+              + m.album.title + ' : ' + 
+              m.artist.name + '</p> <p class="preview">' + 
+              m.preview + '</p> </div>').join("<br>");
+    }
     
-    });
+    );
 });
 
 
