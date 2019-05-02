c52ea4d37cc1a2ddc40e3c4c25b44e17b6bb9c57
T-M-N <naidutania@outlook.fr>
2019-05-02 21:06:04 +0200

    Update favoris.js

--- js/favoris.js
+++ js/favoris.js
@@ -6,27 +6,29 @@ $(document).ready(function () {
   for (let m of tab) {
     $(`<div class="container">
       <div  class="main">
-          <p class="album">
-              <img width="200px" src="${m.album.cover_big}"/><br>
-              <span class="nomArtiste">Artiste : ${m.artist.name}</span><br>
-              <span class="nomArtiste">Titre : ${m.title}</span><br>
-              <span class="nomArtiste">Album : ${m.album.title}</span><br>
-              <p class="preview">
-                  <audio controls src="${m.preview}"></audio>
-              </p><br>
+          <div class="album">
+             <div class="enligne"> <img width="150px" src="${m.album.cover_big}"/><br></div>
+              <div class="enligne"><span class="nomArtiste">Artiste : </span>${m.artist.name}<br>
+              <span class="nomArtiste">Titre : </span>${m.title}<br>
+              <span class="nomArtiste">Album : </span>${m.album.title}<br></div>
+          </div>
+              <div class="preview">
+                  <audio controls src="${m.preview}" width="700" height="350"></audio>
+              </div><br>
               <button type="submit" class="btn-favoris">
                   <i class="fas fa-heart"></i>
                   <span class="text-favoris">Retirer des favoris</span>
               </button>
           </div>
-      </div>`).data('m', m)
-      .appendTo('#afficher');
+      </div>`
+    ).data('m', m)
+    .appendTo('#afficher');
   }
 
-
+    
   $("#afficher").on("click", ".btn-favoris", function () {
     //$(this).html("retirer");
-    alert("Recharger la page");
+     alert("Recharger la page");
     let m = $(this).parents('.container').data('m');
 
     //console.log('récupéré', m);
