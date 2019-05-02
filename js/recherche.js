3d7132b19b27e2963b72552b5661ba59fdff505e
T-M-N <naidutania@outlook.fr>
2019-05-02 16:11:39 +0200

    Update recherche.js

--- js/recherche.js
+++ js/recherche.js
@@ -3,8 +3,8 @@
 const FormulaireRecherche = $('#FormulaireRecherche');
 const SelectBox = $('#selectBox');
 
-FormulaireRecherche.on('submit', function(event) {
-event.preventDefault();
+FormulaireRecherche.on('submit', function (event) {
+    event.preventDefault();
 
     //INPUT DE RECHERCHE
     const recherche = FormulaireRecherche.find('#recherche');
@@ -16,41 +16,74 @@ event.preventDefault();
     console.log(selectValeur);
 
     $.ajax({
-        url : 'https://api.deezer.com/search?q='+rechercheValeur+'&order='+selectValeur+'&output=jsonp',
-        dataType : 'jsonp'
-    }).done(function(musiques) {
-    console.log(musiques);
-        document.querySelector('#results').innerHTML = 
-            musiques.data.map(
-                        m =>
-                            '<div class="main"><p class="album">'
-                            + '<img width="200px" src="'
-                            + m.album.cover_big+ '"/>' 
-                            + '<br>'
-                            + m.artist.name 
-                            + ' : <br>' 
-                            + m.album.title 
-                            + '</p> <br> <p class="preview">' 
-                            + '<audio controls src="'
-                            + m.preview 
-                            +'" ></audio></p><br><button class="btn-favoris"><i class="fas fa-heart"></i>  <span class="text-favoris">Ajouter au favoris</span></button><button class="btn-favoris display-favoris"><i class="fas fa-heart"></i>  <span class="text-favoris">Retirer au favoris</span></button></div>'
-                            ).join("<br>");
+        url: 'https://api.deezer.com/search?q=' + rechercheValeur + '&order=' + selectValeur + '&output=jsonp',
+        dataType: 'jsonp'
+    }).done(function (musiques) {
+        console.log(musiques);
 
-                            //MODIFICATION TEXTE APRES CLICK
-                   /* $(".btn-favoris").click(function(){  
-                             $(this).html(' Retirer du favoris'); //console.log("test");
-                    });*/
-                    
-            /*$(".btn-favoris").click(function(){ 
-                $(this).text(function(i, text){
-                    return text === "Ajouter aux favoris" ? "Retirer des favoris" : "Ajouter aux favoris";
-                });      
-                
-            });*/
+        let template = $(document.createDocumentFragment());
+        for (let i = 0; i < musiques.data.length; i++) {
+            let m = musiques.data[i];
+
+            //console.log('musique', i, m);   
+
+            $(`<div class="container">
+                    <div  class="main">
+                        <p class="album">
+                            <img width="200px" src="${m.album.cover_big}"/><br>
+                            <span class="nomArtiste">Artiste : ${m.artist.name}</span><br>
+                            <span class="nomArtiste">Titre : ${m.title}</span><br>
+                            <span class="nomArtiste">Album : ${m.album.title}</span><br>
+                            <p class="preview">
+                                <audio controls src="${m.preview}"></audio>
+                            </p><br>
+                            <button type="submit" class="btn-favoris">
+                                <i class="fas fa-heart"></i>
+                                <span class="text-favoris">Ajouter au favoris</span>
+                            </button>
+                        </div>
+                    </div>`)
+                .data('m', m)
+                .appendTo(template);
         }
-    );
 
+        $('#results').html(template); // ...
+    });
 
 });
 
+$("#results").on("click", ".btn-favoris", function () {
+    ("vous avez ajouté une musique au favoris");
+    let m = $(this).parents('.container').data('m');
 
+    console.log('récupéré', m);
+
+    let tab = [];
+
+    if (localStorage.getItem('listM') == null) {
+        alert("c'est vide");
+        tab.push(m);
+        localStorage.setItem('listM', JSON.stringify(tab));
+   
+    }else {
+        tab = JSON.parse(localStorage.getItem('listM'));
+        tab.push(m);
+        localStorage.setItem('listM', JSON.stringify(tab));
+
+        for (let i = 0; i < tab.length; i++) {
+            if (tab[i].id === m.id) {
+                alert("il est déjà dans les favoris");
+                tab.splice(i, 1);
+                i--;
+            }else{
+                alert("il n'est pas dans vos favoris");
+            }
+            // else{
+            //     tab.push(m);
+            //     localStorage.setItem('listM', JSON.stringify(tab));
+            // }
+        }
+    }
+
+    
+});
