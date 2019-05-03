b448ed6cb440da943e6df5338e18d0463f93c1b9
T-M-N <naidutania@outlook.fr>
2019-05-03 12:46:17 +0200

    Update recherche.js

--- js/recherche.js
+++ js/recherche.js
@@ -29,15 +29,15 @@ FormulaireRecherche.on('submit', function (event) {
 
             $(`<div class="container">
                     <div  class="main">
-                        <div class="album">
-                            <img width="200px" src="${m.album.cover_big}"/><br>
-                            <span class="nomArtiste">Artiste : ${m.artist.name}</span><br>
-                            <span class="nomArtiste">Titre : ${m.title}</span><br>
-                            <span class="nomArtiste">Album : ${m.album.title}</span><br>
-                        </div>
-                         <div class="preview">
-                                <audio controls src="${m.preview}"></audio>
-                        </div><br>
+                    <div class="album">
+                        <div class="enligne"> <img width="150px" src="${m.album.cover_big}"/><br></div>
+                        <div class="enligne"><span class="nomArtiste">Artiste : </span>${m.artist.name}<br>
+                        <span class="nomArtiste">Titre : </span>${m.title}<br>
+                        <span class="nomArtiste">Album : </span>${m.album.title}<br></div>
+                    </div>
+                    <div class="preview">
+                        <audio controls src="${m.preview}" width="700" height="350"></audio>
+                    </div><br>
                             <button type="submit" class="btn-favoris">
                                 <i class="fas fa-heart"></i>
                                 <span class="text-favoris">Ajouter au favoris</span>
@@ -56,30 +56,37 @@ FormulaireRecherche.on('submit', function (event) {
 
 $("#results").on("click", ".btn-favoris", function () {
 
-    let m = $(this).parents('.container').data('m');
+    let $button = $(this);
+    let m = $button.parents('.container').data('m');
 
     console.log('récupéré', m);
 
     let tab = [];
-
     if (localStorage.getItem('listM') == null) {
         alert("c'est vide");
 
         tab.push(m);
         localStorage.setItem('listM', JSON.stringify(tab));
-    } else {
-        // for (let i = 0; i < tab.length; i++) {
-        //     if (tab[i].id === m.id) {
-        //         tab = JSON.parse(localStorage.getItem('listM'));
-        //         tab.push(m);
-        //         localStorage.setItem('listM', JSON.stringify(tab));
-        //     }
-        // }
+    } 
+    else {
         tab = JSON.parse(localStorage.getItem('listM'));
-        tab.push(m);
+
+        let supprime = false;
+
+        for (let i = 0; i < tab.length; i++) {
+            // Si l'élément cliqué était déjà en favoris, on l'en supprime
+            if (tab[i].id === m.id) {
+               tab.splice(i, 1);
+               supprime = true;
+               break;
+            }
+        }
+       
+        // Si l'élément cliqué n'était pas encore en favoris, on l'ajoute
+        if (supprime === false) {
+            tab.push(m);
+            $button.text('SUPPRIMER DES FAVS');
+        }
         localStorage.setItem('listM', JSON.stringify(tab));
     }
-
-
-
 });
