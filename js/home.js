ec9b50066db4d2e5acaf88eec262f8adaa46742d
T-M-N <naidutania@outlook.fr>
2019-05-03 12:46:15 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -2,61 +2,44 @@ $(document).ready(function () {
     $("#hasard").html(
         `<button type="submit" class="btn-refresh"><i class="fas fa-heart-broken"></i> Choisir une autre musique</button>`
     );
+
     function aleatoire() {
-      
-            let tab = JSON.parse(localStorage.getItem('listM')) || [];
-            
-            let index = Math.floor(Math.random() * tab.length);
-            
-            if (tab) { 
+
+        let tab = JSON.parse(localStorage.getItem('listM')) || [];
+
+        let index = Math.floor(Math.random() * tab.length);
+
+        if (tab) {
+            $('.container').remove();
 
             let m = tab[index];
 
             $(`<div class="container">
                     <div  class="main">
-                        <div class="album">
-                            <img width="200px" src="${m.album.cover_big}"/><br>
-                            <span class="nomArtiste">Artiste : ${m.artist.name}</span><br>
-                            <span class="nomArtiste">Titre : ${m.title}</span><br>
-                            <span class="nomArtiste">Album : ${m.album.title}</span><br>
-                        </div>
-                         <div class="preview">
-                                <audio controls src="${m.preview}"></audio>
-                        </div><br>
+                    <div class="album">
+                        <div class="enligne"> <img width="150px" src="${m.album.cover_big}"/><br></div>
+                        <div class="enligne"><span class="nomArtiste">Artiste : </span>${m.artist.name}<br>
+                        <span class="nomArtiste">Titre : </span>${m.title}<br>
+                        <span class="nomArtiste">Album : </span>${m.album.title}<br></div>
+                     </div>
+                     <div class="preview">
+                         <audio controls src="${m.preview}" width="700" height="350"></audio>
+                     </div><br>
                         <button type="submit" class="btn-favoris">
-                        <i class="fas fa-heart"></i>
+                        <i class="fas fa-heart-broken"></i>
                         <span class="text-favoris">Retirer des favoris</span>
                     </button>
                         </div>
                     </div>`)
-                    .data('m', m)
-                    .prependTo("#hasard");
-           	}
-           
-        
+                .data('m', m)
+                .prependTo("#hasard");
+        }
+
+
     }
 
     aleatoire();
-    
-    $("#hasard").on("click", ".btn-favoris", function () {
-        //$(this).html("retirer");
-         alert("Recharger la page");
-        let m = $(this).parents('.container').data('m');
-    
-        //console.log('récupéré', m);
-    
-        let tab = JSON.parse(localStorage.getItem('listM')) || [];
-    
-        for (let i = 0; i < tab.length; i++) {
-          if (tab[i].id === m.id) {
-            tab.splice(i, 1);
-            i--;
-            // localStorage.removeItem('listM');
-          }
-        }
-    
-        localStorage.setItem('listM', JSON.stringify(tab));
-    
-      });
+
+    $("#hasard").on("click",  aleatoire);
 
 });
