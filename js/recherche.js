fdc607a92e9d73eb3103aa7f9e869e60aee1ed97
T-M-N <naidutania@outlook.fr>
2019-05-03 16:23:18 +0200

    Update recherche.js

--- js/recherche.js
+++ js/recherche.js
@@ -1,33 +1,25 @@
-'user strict';
+'use strict';
+$(document).ready(function () {
+    const FormulaireRecherche = $('#FormulaireRecherche');
+    const SelectBox = $('#selectBox');
 
-const FormulaireRecherche = $('#FormulaireRecherche');
-const SelectBox = $('#selectBox');
+    FormulaireRecherche.on('submit', function (event) {
+        event.preventDefault();
 
-FormulaireRecherche.on('submit', function (event) {
-    event.preventDefault();
+        const recherche = FormulaireRecherche.find('#recherche');
+        const rechercheValeur = recherche.val();
 
-    //INPUT DE RECHERCHE
-    const recherche = FormulaireRecherche.find('#recherche');
-    const rechercheValeur = recherche.val();
+        const selectOption = SelectBox.find('option:selected');
+        const selectValeur = selectOption.val();
 
-    //SELECT BOX
-    const selectOption = SelectBox.find('option:selected');
-    const selectValeur = selectOption.val();
-    console.log(selectValeur);
-
-    $.ajax({
-        url: 'https://api.deezer.com/search?q=' + rechercheValeur + '&order=' + selectValeur + '&output=jsonp',
-        dataType: 'jsonp'
-    }).done(function (musiques) {
-        console.log(musiques);
-
-        let template = $(document.createDocumentFragment());
-        for (let i = 0; i < musiques.data.length; i++) {
-            let m = musiques.data[i];
-
-            //console.log('musique', i, m);   
-
-            $(`<div class="container">
+        $.ajax({
+            url: 'https://api.deezer.com/search?q=' + rechercheValeur + '&order=' + selectValeur + '&output=jsonp',
+            dataType: 'jsonp'
+        }).done(function (musiques) {
+            let template = $(document.createDocumentFragment());
+            for (let i = 0; i < musiques.data.length; i++) {
+                let m = musiques.data[i];
+                $(`<div class="container">
                     <div  class="main">
                     <div class="album">
                         <div class="enligne"> <img width="150px" src="${m.album.cover_big}"/><br></div>
@@ -38,55 +30,18 @@ FormulaireRecherche.on('submit', function (event) {
                     <div class="preview">
                         <audio controls src="${m.preview}" width="700" height="350"></audio>
                     </div><br>
-                            <button type="submit" class="btn-favoris">
-                                <i class="fas fa-heart"></i>
-                                <span class="text-favoris">Ajouter au favoris</span>
+                            <button type="submit" class="btnFavoris">
+                                <i class="fas fa-heart"></i> Ajouter au favoris
                             </button>
                         </div>
                     </div>`)
-                .data('m', m)
-                .appendTo(template);
-        }
+                    .data('m', m)
+                    .appendTo(template);
+            }
+            $('#results').html(template);
+        });
 
-        $('#results').html(template); // ...
     });
 
-});
-
-
-$("#results").on("click", ".btn-favoris", function () {
-
-    let $button = $(this);
-    let m = $button.parents('.container').data('m');
-
-    console.log('récupéré', m);
-
-    let tab = [];
-    if (localStorage.getItem('listM') == null) {
-        alert("c'est vide");
-
-        tab.push(m);
-        localStorage.setItem('listM', JSON.stringify(tab));
-    } 
-    else {
-        tab = JSON.parse(localStorage.getItem('listM'));
-
-        let supprime = false;
-
-        for (let i = 0; i < tab.length; i++) {
-            // Si l'élément cliqué était déjà en favoris, on l'en supprime
-            if (tab[i].id === m.id) {
-               tab.splice(i, 1);
-               supprime = true;
-               break;
-            }
-        }
-       
-        // Si l'élément cliqué n'était pas encore en favoris, on l'ajoute
-        if (supprime === false) {
-            tab.push(m);
-            $button.text('SUPPRIMER DES FAVS');
-        }
-        localStorage.setItem('listM', JSON.stringify(tab));
-    }
+    $("#results").on("click", ".btnFavoris", fonctionBtnRetirerRecherche);
 });
